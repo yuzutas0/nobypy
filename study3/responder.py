19ab86902e8e730d569d68ac46d090a886284f4b
yuzutas0 <yuzutas0@users.noreply.github.com>
2018-05-26 16:09:03 +0900

    [WIP] chapter7-4

--- study3/responder.py
+++ study3/responder.py
@@ -3,6 +3,7 @@
 import random
 import re
 from dictionary import Dictionary
+from morph import Morph
 
 
 class Responder:
@@ -11,7 +12,7 @@ class Responder:
     def __init__(self, name):
         self.name = name
     
-    def response(self, input_text, mood):
+    def response(self, input_text='', tokens=[], mood=0):
         return ''
 
 
@@ -33,4 +34,18 @@ class PatternResponder(Responder):
                 resp = item.choise(mood)
                 if resp != None:
                     return resp.replace('%match%', m.group(0))
+        return random.choice(self.dictionary.random)
+
+
+class TemplateResponder(Responder):
+    def response(self, input_text, tokens, mood):
+        keywords = []
+        for token in tokens:
+            if Morph.is_keyword(token):
+                keywords.append(token.surface)
+        count = len(keywords)
+        if count > 0 and count in self.dictionary.template.keys():
+            template = random.choice(self.dictionary.template[count])
+            template = template.replace('%noun%', '%s')
+            return template % keywords
         return random.choice(self.dictionary.random)
