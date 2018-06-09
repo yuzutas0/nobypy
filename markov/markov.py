91011e2f71f1903088f6f09a00ce612106df987b
yuzutas0 <yuzutas0@users.noreply.github.com>
2018-06-09 18:33:01 +0900

    [WIP] chapter8-2

--- markov/markov.py
+++ markov/markov.py
@@ -2,6 +2,7 @@
 
 import copy
 import random
+import pickle
 
 
 class Markov:
@@ -19,22 +20,21 @@ class Markov:
         parts = copy.deepcopy(parts)
         prefix1 = parts.pop(0)
         prefix2 = parts.pop(0)
-        self.add_start(prefix1)
+        self.__add_start(prefix1)
         
         for suffix in parts:
-            self.add_suffix(prefix1, prefix2, suffix)
+            self.__add_suffix(prefix1, prefix2, suffix)
             prefix1 = prefix2
             prefix2 = suffix
         
-        add_suffix(prefix1, prefix2, self.end_mark)
+        __add_suffix(prefix1, prefix2, self.end_mark)
     
     def generate(self, keyword):
         if len(self.dic) == 0:
             return None
-        
         words = []
         
-        prefix1 = keyword if keyword in self.dic.keys() else self.select_start()
+        prefix1 = keyword if keyword in self.dic.keys() else self.__select_start()
         prefix2 = random.choice(self.dic[prefix1].keys())
         words.extend([prefix1, prefix2])
         
@@ -47,3 +47,33 @@ class Markov:
             prefix2 = suffix
         
         return ''.join(words)
+    
+    # TODO: filenameが同じなのは不自然?
+    def load(self, f):
+        # f = open(filename,'rb')
+        self.dic = pickle.load(f)
+        self.starts = pickle.load(f)
+        # f.close()
+
+    # TODO: filenameが同じなのは不自然?
+    def save(self, f):
+        # f = open(filename,'wb')
+        pickle.dump(self.dic, f)
+        pickle.dump(self.starts, f)
+        # f.close
+
+    # private method
+    def __add_suffix(self, prefix1, prefix2, suffix):
+        if prefix1 not in self.dic.keys():
+            self.dic[prefix1] = {}
+        if prefix2 not in self.dic[prefix1].keys():
+            self.dic[prefix1][prefix2] = []
+        self.dic[prefix1][prefix2].append(suffix)
+
+    def __add_start(self, prefix1):
+        if prefix1 not in self.starts.keys():
+            self.starts[prefix1] = 0
+        self.starts[prefix1] += 1
+
+    def __select_start(self):
+        random.choice(self.starts.keys())
